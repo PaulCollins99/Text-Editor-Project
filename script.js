1b0c9476763712de3174db5304b8cd9156f0a8de
PaulCollins99 <paul.collins@node-it.com>
2019-10-10 16:41:36 +0100

    added event listeners and boot functions

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-///JavaScript///
+'use strict';
+
+function click () {
+    console.log("This is a test listener");
+    
+}
+
+function boot () {
+    window.left.addEventListener('click', click)
+    window.right.addEventListener('click', click)
+    window.up.addEventListener('click', click)
+    window.down.addEventListener('click', click)
+}
+
+window.addEventListener('load', boot);
