688b41498598218ad6b4ee5fc65d0ce7c64ae5e8
PaulCollins99 <paul.collins@node-it.com>
2019-10-11 16:26:44 +0100

    update new script

---
+++ script.js
@@ -0,0 +1,24 @@
+'use strict';
+function click (e) {
+    console.log(e.target.id);
+    
+}
+
+function convertToArray (e) {
+    let stringArray = document.getElementById("mainTextArea").value.split("\n");
+    console.log(stringArray);
+
+    
+    
+}
+
+function boot () {
+    window.left.addEventListener('click', click);
+    window.right.addEventListener('click', click);
+    window.up.addEventListener('click', click);
+    window.down.addEventListener('click', click);
+    window.mainAdd.addEventListener('click', click);
+    window.testButton.addEventListener('click', convertToArray);
+}
+
+window.addEventListener('load', boot);
