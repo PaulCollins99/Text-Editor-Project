659077d96ee35052ce0d25fbf8eb1d685f9cdaf9
PaulCollins99 <paul.collins@node-it.com>
2019-10-11 17:16:03 +0100

    add indent function

--- script.js
+++ script.js
@@ -6,8 +6,36 @@ function click (e) {
 
 function convertToArray (e) {
     let stringArray = document.getElementById("mainTextArea").value.split("\n");
-    console.log(stringArray);
+    console.log(getLineNumber());
+    
+    return stringArray;
 }
+
+function getLineNumber () {
+    return window.mainTextArea.value.substring(0,window.mainTextArea.selectionStart).split("\n").length;
+}
+
+function keydownHandler (e) {
+   
+    if (e.code == "Tab") {
+        addIndent();
+    }
+    
+}
+
+function addIndent () {
+    let element = document.getElementById("mainTextArea");
+    let lineNumber = getLineNumber();
+    let stringArray = convertToArray(element);
+
+    element.value = "";
+    stringArray[lineNumber - 1] = "   " + stringArray[lineNumber - 1];
+    for (const x in stringArray) {
+        element.value += stringArray[x];
+    }
+}
+
+
 
 function boot () {
     window.left.addEventListener('click', click);
@@ -16,6 +44,7 @@ function boot () {
     window.down.addEventListener('click', click);
     window.mainAdd.addEventListener('click', click);
     window.testButton.addEventListener('click', convertToArray);
+    window.mainTextArea.addEventListener('keydown', keydownHandler)
 }
 
 window.addEventListener('load', boot);
