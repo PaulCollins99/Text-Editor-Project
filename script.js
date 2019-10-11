a8740f922c67f30d2eea87dba39159e73be722d7
PaulCollins99 <paul.collins@node-it.com>
2019-10-11 12:59:17 +0100

    added some js

--- script.js
+++ script.js
@@ -5,12 +5,24 @@ function click () {
     
 }
 
+function createButton () {
+
+    let newButton = document.createElement('button');
+    newButton.setAttribute(type="button");
+    let location = document.getElementById('navigation')
+    insertAfter(newButton, location);
+}
+
+function insertAfter(newNode, referenceNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+
 function boot () {
-    window.left.addEventListener('click', click)
-    window.right.addEventListener('click', click)
-    window.up.addEventListener('click', click)
-    window.down.addEventListener('click', click)
-    window.mainAdd.addEventListener('click', click)
+    window.left.addEventListener('click', click);
+    window.right.addEventListener('click', click);
+    window.up.addEventListener('click', click);
+    window.down.addEventListener('click', click);
+    window.mainAdd.addEventListener('click', click);
 }
 
 window.addEventListener('load', boot);
