13d9993a57ae7af88b926dfe654302bac02d747c
PaulCollins99 <paul.collins@node-it.com>
2019-12-03 15:13:37 +0000

    update server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static("."))
+app.use('/', express.static('files', {extensions: ['htm', 'html']}));
 
 app.get("*", function (req, res) {
   res.end("");
