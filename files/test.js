cfd1e1a26b48c5700df404c7167c092bc75ce2f5
PaulCollins99 <paul.collins@node-it.com>
2019-12-06 13:31:44 +0000

    kapow

---
+++ files/test.js
@@ -0,0 +1,4 @@
+// eslint-disable-next-line no-undef
+QUnit.test('Good Test', (assert) => {
+  assert.ok(getLineNumber(), 'The function must return something');
+});
--- test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-// eslint-disable-next-line no-undef
-QUnit.test('test', (assert) => {
-  const value = 'hello';
-  assert.equal(value, 'hello', 'excpecetd value is hello');
-});
